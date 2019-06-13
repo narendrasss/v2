c4f2020a61bf11cc22210faf1bad975d167c0417
Narendras <n.syahrasyad.n@gmail.com>
2019-06-13 13:09:31 -0700

    Fixed theme being undefined during build

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         path: `${__dirname}/content/projects/`,
       },
     },
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-transformer-remark`,
